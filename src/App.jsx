793532dc8dd841f6550c838452e0dd8f8f8d7271
Marwan KN <marwannofal@gmail.com>
2023-10-16 10:09:02 -0400

    Install all dependecies needed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import logo from "./assets/BloodType.gif";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Hello</h1>
-      <img src={logo} alt="loading..." />
-    </>
+    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
   );
 }
 
